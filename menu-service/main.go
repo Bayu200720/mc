98affb440672eb5e5fe8be94ec197c77fb094cd4
bayu <bayuyudo20@gmail.com>
2020-10-15 23:29:20 +0700

    ok

--- menu-service/main.go
+++ menu-service/main.go
@@ -1,13 +1,18 @@
 package main
 
-import(
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/Bayu200720/mc/menu-service/handler"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
 
-	router.Handle("/admin-auth", http.HandlerFunc(handler.ValidateAuth))
+	router.Handle("/admin-auth", http.HandlerFunc(handler.AddMenu))
 
 	fmt.Printf("Auth service listen on :8001")
 	log.Panic(http.ListenAndServe(":8001", router))
